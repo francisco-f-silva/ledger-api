633ab01f12fc572950866b278abc4cbf37c6eecc
Francisco Silva <franciscocpfsilva@gmail.com>
2025-08-22 15:30:43 +0100

    Add base maven project

---
+++ src/main/java/io/github/francisco_f_silva/ledger_api/LedgerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.francisco_f_silva.ledger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/francisco_f_silva/ledger_api/LedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.francisco_f_silva.ledger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
